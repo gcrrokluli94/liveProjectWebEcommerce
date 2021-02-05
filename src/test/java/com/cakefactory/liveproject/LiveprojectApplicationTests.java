f62c11a1c44e6cabbfee1197b2e27903913f71f4
Rrok <rrokluli@gmail.com>
2021-02-05 19:20:18 +0100

    Bootstrap the Project

---
+++ src/test/java/com/cakefactory/liveproject/LiveprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cakefactory.liveproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
